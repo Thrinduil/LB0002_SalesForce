c54567e56c04bcdf9977cbba23966429a2f2d057
Fredrik Söderquist <fredrik@fredriksoderquist.se>
2020-02-29 11:48:49 +0100

    Add GetSalesPerson

--- SalesForce/Program.cs
+++ SalesForce/Program.cs
@@ -8,10 +8,14 @@ namespace SalesForce
         {
             Console.WriteLine("Hello World!");
 
-            SalesPerson kalle = new SalesPerson("Kalle Anka", "0101010001", "Ankeborg", 123);
-            Console.WriteLine(kalle);
+            SalesPerson Kalle = new SalesPerson("Kalle Anka", "0101010001", "Ankeborg", 123);
+            Console.WriteLine(Kalle);
+
+            SalesPerson Joakim = SalesPerson.GetSalesPerson();
+            Console.WriteLine(Joakim);
+
             Console.ReadLine();
-        }
+        }        
     }
 
     public class SalesPerson
@@ -23,7 +27,7 @@ namespace SalesForce
         readonly int soldArticles;
 
         // Konstruktor
-        public SalesPerson ( string name, string personalCodeNumber, string district, int soldArticles)
+        public SalesPerson(string name, string personalCodeNumber, string district, int soldArticles)
         {
             this.name = name;
             this.personalCodeNumber = personalCodeNumber;
@@ -32,6 +36,23 @@ namespace SalesForce
         }
 
         // Metoder
+        public static SalesPerson GetSalesPerson()
+        {
+            Console.WriteLine("Namn:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Personnummer:");
+            string personalCodeNumber = Console.ReadLine();
+
+            Console.WriteLine("Distrikt:");
+            string district = Console.ReadLine();
+
+            Console.WriteLine("Antal sålda artiklar:");
+            int soldArticles = Convert.ToInt32(Console.ReadLine());
+
+            return new SalesPerson(name, personalCodeNumber, district, soldArticles);
+        }
+
         public override string ToString()
         {
             return name + "\t" + personalCodeNumber + "\t" + district + "\t" + soldArticles;

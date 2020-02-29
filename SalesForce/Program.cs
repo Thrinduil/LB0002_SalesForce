c1d4371f719bf431a6e02b099d1998bd117185e4
Fredrik Söderquist <fredrik@fredriksoderquist.se>
2020-02-29 11:27:45 +0100

    Implement SalesPerson class

--- SalesForce/Program.cs
+++ SalesForce/Program.cs
@@ -7,6 +7,34 @@ namespace SalesForce
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            SalesPerson kalle = new SalesPerson("Kalle Anka", "0101010001", "Ankeborg", 123);
+            Console.WriteLine(kalle);
+            Console.ReadLine();
+        }
+    }
+
+    public class SalesPerson
+    {
+        // Medlemsvariabler
+        readonly string name;
+        readonly string personalCodeNumber;
+        readonly string district;
+        readonly int soldArticles;
+
+        // Konstruktor
+        public SalesPerson ( string name, string personalCodeNumber, string district, int soldArticles)
+        {
+            this.name = name;
+            this.personalCodeNumber = personalCodeNumber;
+            this.district = district;
+            this.soldArticles = soldArticles;
+        }
+
+        // Metoder
+        public override string ToString()
+        {
+            return name + "\t" + personalCodeNumber + "\t" + district + "\t" + soldArticles;
         }
     }
 }
